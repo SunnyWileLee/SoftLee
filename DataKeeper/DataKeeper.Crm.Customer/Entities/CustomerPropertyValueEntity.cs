c555fdc89238eac204a50f390ac2cf994217506c
liming <lovetioner@163.com>
2018-03-07 23:11:33 +0800

    1
    Signed-off-by: liming <lovetioner@163.com>

---
+++ DataKeeper/DataKeeper.Crm.Customer/Entities/CustomerPropertyValueEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataKeeper.Crm.Customer.Entities
+{
+    class CustomerPropertyValueEntity
+    {
+        public Guid CustomerId { get; set; }
+        public Guid PropertyId { get; set; }
+        public string Value { get; set; }
+    }
+}
--- DataKeeper/DataKeeper.Oms.Order/Class1.cs
+++ DataKeeper/DataKeeper.Oms.Order/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataKeeper.Framework
+namespace DataKeeper.Oms.Order
 {
     public class Class1
     {
