82d41cfe208465df82daa4c7ccb59683abe85099
liming <lovetioner@163.com>
2018-04-17 22:11:24 +0800

    前端控制台
    Signed-off-by: liming <lovetioner@163.com>

--- DataKeeper/DataKeeper.Console/Scripts/common.js
+++ DataKeeper/DataKeeper.Console/Scripts/common.js
@@ -1,10 +1,13 @@
 ﻿function post(url, data, success, error) {
-    var host = 'http://localhost:4345/';
+    var host = 'http://localhost:60747/';
     $.ajax({
         url: host + url,
         type: "POST",
         contentType: "application/json",
         data: data,
+        headers: {
+            token: '8FF40BAF-CF97-445B-BD40-D00466E63C03'
+        },
         success: function (response) {
             if (success == null) {
                 return false;
@@ -21,10 +24,13 @@
 }
 
 function get(url, success, error) {
-    var host = 'http://localhost:4345/';
+    var host = 'http://localhost:60747/';
     $.ajax({
         url: host + url,
         type: "GET",
+        headers: {
+            token:'8FF40BAF-CF97-445B-BD40-D00466E63C03'
+        },
         success: function (response) {
             if (success == null) {
                 return false;
