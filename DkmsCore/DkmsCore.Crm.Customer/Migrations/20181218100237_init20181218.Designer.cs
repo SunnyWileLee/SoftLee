9e58bc4a3aa188a3e05877fd6ece1e3368e6565a
Lancer <493261380@qq.com>
2018-12-18 18:33:46 +0800

    customer同步到mysql数据库
    Signed-off-by: Lancer <493261380@qq.com>

--- DkmsCore/DkmsCore.Crm.Customer/Migrations/20181218100237_init20181218.Designer.cs
+++ DkmsCore/DkmsCore.Crm.Customer/Migrations/20181218100237_init20181218.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DkmsCore.Crm.Customer.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DkmsCore.Crm.Customer.Migrations
 {
     [DbContext(typeof(CrmCustomerDbContext))]
-    partial class CrmCustomerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181218100237_init20181218")]
+    partial class init20181218
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DkmsCore/DkmsCore.Crm.Customer/Migrations/20181218100237_init20181218.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DkmsCore.Crm.Customer.Migrations
+{
+    public partial class init20181218 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
